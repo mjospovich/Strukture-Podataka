5442632b6db39c12e8e926612cf1900f2ac64f98
Martin <93053179+mjospovich@users.noreply.github.com>
2022-11-05 22:10:12 +0100

    started with task 4

--- 4_vjezbe/zad_4.c
+++ 4_vjezbe/zad_4.c
@@ -12,7 +12,7 @@
 #define MAX_BUFFER_SIZE (1024)
 #define MAX_FILE_NAME (128)
 #define MAX_LINE (128)
-#define MAX_POLYNOMIAL (32)
+#define MAX_POLYNOMIAL (16)
 
 
 struct _Polynomial;
@@ -30,15 +30,29 @@ typedef struct _Polynomial{
 
 //function definitions
 int insert_from_file(char*, Polynomial*);
-int insert_sorted(Polynomial**);
 
 
 int main(void)
 {
-    Polynomial** head_all;
-    head_all = (Polynomial**)calloc(MAX_POLYNOMIAL, sizeof(Polynomial));
+    Polynomial* head_arr;
+    char fileName[MAX_FILE_NAME] = {"\0"};
+    int check_msg = 0;
+    
+    //allocating an array of heads
+    head_arr = (Polynomial*)calloc(MAX_POLYNOMIAL, sizeof(Polynomial));
+    //checking if memory has allocated correctly
+    if (head_arr == NULL)
+    {
+        printf("Memory not allocated, exiting the program!\n");
+        return MEM_ALLOC_ERROR;
+    }
 
-    while
+    printf("Enter file name!\n");
+    scanf("%s", fileName);
+
+    check_msg = insert_from_file(fileName, head_arr);
+
+
 
 
 
@@ -46,16 +60,20 @@ int main(void)
     return SUCCESS;
 }
 
-int insert_from_file(char* fileName, Polynomial* head)
+int insert_from_file(char* fileName, Polynomial* head_arr)
 {
     int multiplier = 0;
-    int exp = 0; 
+    int exp = 0;
+    int count_read = 0;
+    int i = 0;
+    int buffer_len;
 
     //opening the text file and setting it to a variable
     FILE *fp = NULL;
     fp = fopen(fileName, "r");
 
     char buffer[MAX_LINE] = { 0 };
+    char temp[MAX_LINE] = { 0 };
 
   
     //checking if the file opened correctly, if not returning error
@@ -68,13 +86,25 @@ int insert_from_file(char* fileName, Polynomial* head)
     //going through the file and finding info about members
     while(!feof(fp))
     {
+        i = 0;
+        count_read = 0;
+
         fgets(buffer, MAX_BUFFER_SIZE, fp);
+        buffer_len = strlen(buffer);
+        
 
         //skip empty row
         if ((strcmp("\n", buffer) != 0))
         {
-            sscanf(buffer, "%s %s %d", name, surname, &birthYear);
-            insert_end(&head, name, surname, birthYear);
+            do
+            {
+                sscanf(temp, "%d %d %n", &multiplier, &exp, &count_read);
+                i = i + count_read;
+                
+
+                
+
+            }while(i != buffer_len);
         }
     }
 
