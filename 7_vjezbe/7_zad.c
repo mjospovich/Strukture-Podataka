6462f660b1e3ef329144f9179b999c7154b37433
Martin <93053179+mjospovich@users.noreply.github.com>
2022-12-10 10:40:49 +0100

    started task 7 and added plan

---
+++ 7_vjezbe/7_zad.c
@@ -0,0 +1,29 @@
+// #pragma warning (disable: 4047)
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#define SUCCESS (0)
+#define MEM_ALLOC_ERROR (-1)
+#define FILE_READ_ERROR (-1)
+#define EMPTY_LIST_WARNING (-1)
+#define MEMBER_NOT_FOUND (-1)
+#define BAD_INPUT (-1)
+#define DEL_ERROR (-1)
+#define FILE_OPENING_ERROR (-1)
+#define MAX_BUFFER_SIZE (1024)
+#define MAX_FILE_NAME (128)
+#define MAX_LINE (128)
+#define INSERTION_ERROR (-1)
+#define OPERATE_ERROR (-1)
+#define STACK_ERROR (-1)
+#define PRINT_ERROR (-1)
+
+
+int main(){
+
+  
+  return SUCCESS;
+}
